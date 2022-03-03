65ae784865a0f07f7c8dfe65587bda5a765f23b1
Marcos Miguel dos Santos <31631847+mmiguel5@users.noreply.github.com>
2022-03-03 16:11:13 -0300

    Depoimento 2
    Depoimento 2

--- src/data/depositions.js
+++ src/data/depositions.js
@@ -5,6 +5,13 @@ export const depositions = [
     description:
       "Dra Lais e a sua secretária estão de parabéns pelo seu atendimento com os pacientes e a delicadeza para com os seus pacientes pois trabalham com dedicação e respeito, pois tem muito amor na sua profissão e com certeza vai muito longe.",
   },
+  {
+    id: 1,
+    name: "Marcelo Albuquerque ",
+    description:
+      "Bom dia, gostaria de relatar minha experiência no consultório. De início fui muito bem recepcionado pela Simone, ela quem me apresentou e me deixou a vontade.",                                  
+      "Pós fui atendido pela Dra Lais, com sua simpatia e esperteza, se faz uma profissional de excelência. Dito então não achei outra forma de ficar muito feliz num geral, os resultados foram evidentes, as queixas que eu tinha foram resolvidas.",
+  },
   // {
   //   id: 2,
   //   name: "Marcos Silva",
