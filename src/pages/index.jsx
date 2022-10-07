bd872ccbd1ba556ebf0b5c4a4696fc5045ecd0ec
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-07 14:16:55 -0300

    replica

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -64,6 +64,23 @@ export default function Home() {
               máxima satisfação aos nossos Clientes.
               Com dúvidas sobre a aplicação de produtos ou serviços elétricos? 
             </p>
+            <p data-aos="fade-up" data-aos-delay="200">
+              Nossa missão é entregar qualidade e preço baixo para proporcionar
+              máxima satisfação aos nossos Clientes.
+              Com dúvidas sobre a aplicação de produtos ou serviços elétricos? 
+            </p>
+            <p data-aos="fade-up" data-aos-delay="200">
+              Nossa missão é entregar qualidade e preço baixo para proporcionar
+              máxima satisfação aos nossos Clientes.
+              Com dúvidas sobre a aplicação de produtos ou serviços elétricos? 
+            </p>
+            <p data-aos="fade-up" data-aos-delay="200">
+              Nossa missão é entregar qualidade e preço baixo para proporcionar
+              máxima satisfação aos nossos Clientes.
+              Com dúvidas sobre a aplicação de produtos ou serviços elétricos? 
+            </p>
+
+
             <div
               className={styles.btnWrap}
               data-aos="fade-up"
