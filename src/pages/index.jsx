5f32ea674fedb45db814958bef61c541aafe058f
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-07 18:59:20 -0300

    index n

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -57,7 +57,7 @@ export default function Home() {
         <div className={`mainContainer ${styles.heroWrap}`}>
           <section className={styles.leftContent}>
             <h1 className="title" data-aos="fade-up">
-              Um atendimento personalizado faz toda a <strong>diferença</strong>
+              Um atendimento personalizado faz toda a <strong2>diferença</strong>
             </h1>
 
           
