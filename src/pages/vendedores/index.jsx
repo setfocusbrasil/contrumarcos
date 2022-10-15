dea08d42c58055ff0d81a0660c9347ca23b664eb
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-15 16:29:22 -0300

    import link

--- src/pages/vendedores/index.jsx
+++ src/pages/vendedores/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { HeaderPage } from "../../components/HeaderPage";
 import { MyImage } from "../../components/MyImage";
 import { Footer } from "../../components/Footer";
