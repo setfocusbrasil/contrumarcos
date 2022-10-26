eb1aaf7684d36fd5ff78cb9e48f782fa390843f5
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-25 22:49:19 -0300

    sss

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -44,8 +44,7 @@ export default function Home() {
         />
         <meta name="author" content="Santa Clara" />
         
-        <script async src="//static.zotabox.com/b/7/b7502fb6f5f765b9614401000a1c1159/widgets.js"></script>
-        
+               
       </Head>
 
       {/* <div className={styles.schedule}>
