8b73bfed86ecd677418ef27f28d8411618a32611
setfocusbrasil <119501939+setfocusbrasil@users.noreply.github.com>
2023-02-13 16:01:11 -0300

    logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -40,8 +40,8 @@ export const Header = () => {
               <Image
                 src="/icons/logo_transparente.png"
                 alt="Eletro Santa Clara"
-                width="180"
-                height="64"
+                width="280"
+                height="124"
               />
             </a>
           </Link>
